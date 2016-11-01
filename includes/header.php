8022116c222bdde8aa6918df356fbfce876afc18
Berzeg07 <berezg86@gmail.com>
2016-11-01 16:30:26 +0300

    17.198

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,26 @@
 <section class="dd-header-wrapper">
-	<div class="dd-line">
-		
-	</div>
+	<div class="dd-line"></div>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12">
+			<div class="col-md-4">
+				<div class="dd-logo-wrap">
+					<div class="dd-logo-img">
+						<img src="img/dd-logo.png" alt="">
+					</div>
+					<div class="dd-logo-txt">
+						<span>Standart</span>
+						<span>Resurs</span>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+			<div class="col-md-4">
 				
 			</div>
+			<div class="col-md-4">
+				
+			</div>
+			<div class="clearfix"></div>
 		</div>
 	</div>
 </section>
