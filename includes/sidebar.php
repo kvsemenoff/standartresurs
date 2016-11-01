c8c6a7671e5b0b47eb62d4686a0105ca74f7b551
hatuhov <hatuhov@yandex.ru>
2016-11-01 23:38:49 +0300

    w

---
+++ includes/sidebar.php
@@ -0,0 +1,322 @@
+
+<div class="clearfix"></div>
+
+<div class="db-sidebar">
+	<ul class="ah-menu-list">
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Сухие смеси, Цемент, Пескобетон</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Штукатурка</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Шпаклевка</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Строительный кирпич</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Бетонные кольца колодезные ЖБИ</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Пеноблоки газосиликатные</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Гипсокартон, Гипсоволокно</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Пазогребневые плиты</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Профиль для гипсокартона</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Утеплитель</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Утеплитель Rockwool</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Утеплитель Пеноплекс</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Утеплитель Пенофол</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Пиломатериалы</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Асбестоцементные изделия</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Грунтовка</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Клей плиточный</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Гидроизоляция</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Клеи монтажный</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Наливные полы</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Пленки</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Сетка металлическая</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Пескобетон (Мансурово)</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Цементно-стружечная плита ЦСП</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Металлопрокат</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+		<li class="ah-menu-list_item">
+			<a href="#" class="ah-menu-list_item-link">Саморезы и гвозди</a>
+			<ul class="ah-submenu-list">
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 1</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 2</a></li>
+				<li class="ah-submenu-list_item"><a href="#" class="ah-submenu-list_item-link">Пункт 3</a></li>
+			</ul>
+		</li>
+	</ul>
+</div>
+
+
+<input type="text" id="minCost" value="0"/>
+<input type="text" id="maxCost" value="1000"/>
+<div id="slider"></div>
+
+<script type="text/javascript">
+
+// jQuery("#slider").slider({
+//     min: 0,
+//     max: 1000,
+//     values: [0,1000],
+//     range: true
+// });
+
+// jQuery("#slider").slider({
+
+//     min: 0,
+
+//     max: 1000,
+
+//     values: [0,1000],
+
+//     range: true,
+
+//     stop: function(event, ui) {
+
+//         jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
+
+//         jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
+
+//     },
+
+//     slide: function(event, ui){
+
+//         jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
+
+//         jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
+
+//     }
+
+// });
+
+// jQuery("input#minCost").change(function(){
+
+//     var value1=jQuery("input#minCost").val();
+
+//     var value2=jQuery("input#maxCost").val();
+
+ 
+
+//     if(parseInt(value1) > parseInt(value2)){
+
+//         value1 = value2;
+
+//         jQuery("input#minCost").val(value1);
+
+//     }
+
+//     jQuery("#slider").slider("values",0,value1);    
+
+// });
+
+ 
+
+     
+
+jQuery("input#maxCost").change(function(){
+
+    var value1=jQuery("input#minCost").val();
+
+    var value2=jQuery("input#maxCost").val();
+
+     
+
+    if (value2 > 1000) { value2 = 1000; jQuery("input#maxCost").val(1000)}
+
+ 
+
+    if(parseInt(value1) > parseInt(value2)){
+
+        value2 = value1;
+
+        jQuery("input#maxCost").val(value2);
+
+    }
+
+    jQuery("#slider").slider("values",1,value2);
+
+});
+
+
+
+
+
+
+
+</script>
+
+
+
+
+
+
+
+
+
+
+<p>Размер рисунка: <input type="range" min="1" max="5" id="size" 
+     oninput="sizePic()" value="3"></p>
+
+<script>
+   function sizePic() {
+    size = document.getElementById("size").value;
+    img = document.getElementById("pic");
+    img.width = 60 + 20*size;
+   }
+  </script>
+
+<script>
+
+$(document).ready(function(){
+	
+	$('.ah-menu-list li').click(function(){
+		$(this).find('.ah-submenu-list').slideToggle(500);
+	});
+
+});
+
+
+</script>
