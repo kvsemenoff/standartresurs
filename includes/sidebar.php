a5bdb6cb2e4e1e9841be279dbf7375436f07f985
hatuhov <hatuhov@yandex.ru>
2016-11-02 10:28:53 +0300

    w

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -191,104 +191,36 @@
 </div>
 
 
-<input type="text" id="minCost" value="0"/>
-<input type="text" id="maxCost" value="1000"/>
+<!-- <input type="text" id="minCost" value="0"/>
+<input type="text" id="maxCost" value="1000"/> -->
+
+
+
 <div id="slider"></div>
 
+
 <script type="text/javascript">
-
-// jQuery("#slider").slider({
-//     min: 0,
-//     max: 1000,
-//     values: [0,1000],
-//     range: true
-// });
-
-// jQuery("#slider").slider({
-
-//     min: 0,
-
-//     max: 1000,
-
-//     values: [0,1000],
-
-//     range: true,
-
-//     stop: function(event, ui) {
-
-//         jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
-
-//         jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
-
-//     },
-
-//     slide: function(event, ui){
-
-//         jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
-
-//         jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
-
-//     }
-
-// });
-
-// jQuery("input#minCost").change(function(){
-
-//     var value1=jQuery("input#minCost").val();
-
-//     var value2=jQuery("input#maxCost").val();
-
- 
-
-//     if(parseInt(value1) > parseInt(value2)){
-
-//         value1 = value2;
-
-//         jQuery("input#minCost").val(value1);
-
-//     }
-
-//     jQuery("#slider").slider("values",0,value1);    
-
-// });
-
- 
-
-     
-
-jQuery("input#maxCost").change(function(){
-
-    var value1=jQuery("input#minCost").val();
-
-    var value2=jQuery("input#maxCost").val();
-
-     
-
-    if (value2 > 1000) { value2 = 1000; jQuery("input#maxCost").val(1000)}
-
- 
-
-    if(parseInt(value1) > parseInt(value2)){
-
-        value2 = value1;
-
-        jQuery("input#maxCost").val(value2);
-
-    }
-
-    jQuery("#slider").slider("values",1,value2);
-
+jQuery("#slider").slider({
+	min: 0,
+	max: 1000,
+	values: [0,1000],
+	range: true
 });
-
-
-
-
-
-
-
 </script>
 
 
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+v
+<br>
 
 
 
@@ -314,6 +246,8 @@ $(document).ready(function(){
 	
 	$('.ah-menu-list li').click(function(){
 		$(this).find('.ah-submenu-list').slideToggle(500);
+
+		return false;
 	});
 
 });
