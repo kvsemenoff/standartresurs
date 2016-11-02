9454832d0f8053b6c3d6b8dd3708eb01703f177c
Залим Лампежев <sabolch@yandex.ru>
2016-11-03 01:13:20 +0300

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -276,3 +276,74 @@
 		</div>
 	</div>
 </section>
+
+<footer class="dg-footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-7">
+				<div class="col-md-4 col-sm-4 col-xs-12">
+					<ul>
+						<li><a href="#">Главная</a></li>
+						<li><a href="#">Магазин</a></li>
+						<li><a href="#">Бренд</a></li>
+						<li><a href="#">Оплата и доставка</a></li>
+					</ul>
+				</div>
+				<div class="col-md-4 col-sm-4 col-xs-12">
+					<ul>
+						<li><a href="#">Интернет-магазин</a></li>
+						<li><a href="#">Новости</a></li>
+						<li><a href="#">Контакты</a></li>
+						<li><a href="#">Информация</a></li>
+					</ul>
+				</div>
+				<div class="col-md-4 col-sm-4 col-xs-12">
+					<ul>
+						<li><a href="#">Наш адрес</a></li>
+						<li><a href="#">Напишите нам</a></li>
+						<li><a href="#">О компании</a></li>
+					</ul>
+				</div>
+				<div class="clear"></div>
+				<div class="leftbottom">
+					<div class="col-md-5 col-sm-5 col-xs-12 logo nopadding">
+						<img src="img/logo-footer.png" alt="">
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-12">
+						<p>Контактные телефоны</p>
+						<span><span class="light">+7 (495)</span> 749 85 35</span><br>
+						<span><span class="light">+7 (919)</span> 997 90 20</span>
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-12 nopadding">
+						<a href="#">Заказать звонок</a>
+					</div>
+				</div>
+			</div>
+			<div class="right-footer col-md-5">
+				<div class="col-md-12">
+					<div class="col-md-12">
+						<span>Наш адрес:</span>
+						<p>65 км. МКАД Строительный комплекс "Синдика". <br>
+						Павильон ОВ-17</p>
+						<div class="cartbox nopadding">
+							<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=MI5JyoYR0DOpkwrjU7tX5TYNo4eEPBXV&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;sourceType=constructor&amp;scroll=true"></script>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="bot">
+		<div class="container">
+			<div class="row relative">
+				<div class="col-md-12">
+					<p>© 2010 - 2016 гг. Стандарт Ресурс <br> 
+					Интернет-магазин строительных материалов</p>
+				</div>
+				<div class="col-md-12 liveint">
+					<a href="#"><img src="img/liveint.png" alt=""></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
