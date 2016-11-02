2a93dc2ea292c06ba1a165ce278e5f9853a0bc6f
Залим Лампежев <sabolch@yandex.ru>
2016-11-02 12:50:47 +0300

    dg-catalog

---
+++ includes/footer.php
@@ -0,0 +1,244 @@
+<section class="dg-catalog">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h3>Каталог строительных материалов</h3>
+
+				<div class="col-md-2 catbox">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat1.png" alt="">
+						</div>
+						<a href="#">Гипсокартон, гипсоволокно</a>
+					</div>
+					
+				</div>
+				<div class="col-md-2 catbox">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat2.png" alt="">
+						</div>
+						<a href="#">Профиль для гипсокартона</a>
+					</div>
+					
+				</div>
+				<div class="col-md-2 catbox">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat3.png" alt="">
+						</div>
+						<a href="#">Цементно-стружечная плита ЦСП</a>
+					</div>
+					
+				</div>
+				<div class="col-md-2 catbox">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat4.png" alt="">
+						</div>
+						<a href="#">Клеи монтажный</a>
+					</div>
+					
+				</div>
+				<div class="col-md-2 catbox">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat5.png" alt="">
+						</div>
+						<a href="#">Пленки</a>
+					</div>
+					
+				</div>
+				<div class="col-md-2 catbox">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat6.png" alt="">
+						</div>
+						<a href="#">Строительный кирпич</a>
+					</div>
+					
+				</div>
+				<div class="col-md-2 catbox">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat7.png" alt="">
+						</div>
+						<a href="#">Сетка металлическая</a>
+					</div>
+					
+				</div>
+				<div class="col-md-2 catbox">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat8.png" alt="">
+						</div>
+						<a href="#">Гидроизоляция</a>
+					</div>
+					
+				</div>
+				<div class="col-md-2 catbox">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat9.png" alt="">
+						</div>
+						<a href="#">Грунтовка</a>
+					</div>
+					
+				</div>
+				<div class="col-md-2 catbox">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat10.png" alt="">
+						</div>
+						<a href="#">Пеноблоки газосиликатные</a>
+					</div>
+					
+				</div>
+				<div class="col-md-2 catbox li3">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat11.png" alt="">
+						</div>
+						<a href="#">Утеплитель</a>
+					</div>
+					<ul>
+						<li><a href="#">Утеплитель Rockwool</a></li>
+						<li><a href="#">Утеплитель Пеноплекс</a></li>
+						<li><a href="#">Утеплитель Пенофол</a></li>
+					</ul>
+				</div>
+				<div class="col-md-2 catbox li3">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat12.png" alt="">
+						</div>
+						<a href="#">Пазогребневые плиты</a>
+					</div>
+					<ul> 
+						<li><a href="#">Волма ПГП</a></li>
+						<li><a href="#">ПГП Кнауф</a></li>
+					</ul>
+				</div>
+				<div class="col-md-2 catbox li3">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat13.png" alt="">
+						</div>
+						<a href="#">Бетонные кольца колодезные ЖБИ</a>
+					</div>
+					<ul>
+						<li><a href="#">Утеплитель Rockwool</a></li>
+						<li><a href="#">Утеплитель Пеноплекс</a></li>
+						<li><a href="#">Утеплитель Пенофол</a></li>
+					</ul>
+				</div>
+				<div class="col-md-2 catbox li3">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat14.png" alt="">
+						</div>
+						<a href="#">Сухие смеси</a>
+					</div>
+					<ul> 
+						<li><a href="#">Евроцемент М500</a></li>
+						<li><a href="#">Евроцемент М300</a></li>
+					</ul>
+				</div>
+				<div class="col-md-2 catbox li3">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat15.png" alt="">
+						</div>
+						<a href="#">Асбестоцементные изделия</a>
+					</div>
+					<ul> 
+						<li><a href="#">Асбестоцементные трубы</a></li>
+						<li><a href="#">Асбестоцементные листы</a></li>
+					</ul>
+				</div>
+				<div class="col-md-2 catbox li11">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat16.png" alt="">
+						</div>
+						<a href="#">Пиломатериалы</a>
+					</div>
+					<ul>
+						<li><a href="#">Деревянные бруски</a></li>
+						<li><a href="#">Доска обрезная</a></li>
+						<li><a href="#">Половая доска</a></li>
+						<li><a href="#">Фанера и ОСБ</a></li>
+						<li><a href="#">Строганная доска</a></li>
+						<li><a href="#">Блок-хаус</a></li>
+					</ul>
+				</div>
+				<div class="col-md-2 catbox li11">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat17.png" alt="">
+						</div>
+						<a href="#">Наливные полы</a>
+					</div>
+					<ul> 
+						<li><a href="#">Наливной пол Cтаратели</a></li>
+						<li><a href="#">Наливной пол Ветонит</a></li>
+						<li><a href="#">Наливной пол Основит</a></li>
+						<li><a href="#">Наливной пол Кнауф</a></li>
+						<li><a href="#">Наливной пол Юнис</a></li>
+					</ul>
+				</div>
+				<div class="col-md-2 catbox li11">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat18.png" alt="">
+						</div>
+						<a href="#">Саморезы и гвозди</a>
+					</div>
+					<ul>
+						<li><a href="#">Анкерный болт 8-10 мм</a></li>
+						<li><a href="#">Анкерный болт 12-20 мм</a></li>
+						<li><a href="#">Гвозди</a></li>
+						<li><a href="#">Дюбель гвозди</a></li>
+						<li><a href="#">Дюбель гвозди утеплителя</a></li>
+						<li><a href="#">Крепеж</a></li>
+						<li><a href="#">Крепежный уголок</a></li>
+						<li><a href="#">Саморезы по дереву</a></li>
+						<li><a href="#">Саморезы по металлу</a></li>
+						<li><a href="#">Саморез с прессшайбой</a></li>
+						<li><a href="#">Шпильки и болты</a></li>
+					</ul>
+				</div>
+				<div class="col-md-2 catbox li11">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat19.png" alt="">
+						</div>
+						<a href="#">Металлопрокат</a>
+					</div>
+					<ul> 
+						<li><a href="#">Арматура А500С</a></li>
+						<li><a href="#">Проволока вязальная</a></li>
+						<li><a href="#">Профильная труба</a></li>
+						<li><a href="#">Профнастил</a></li>
+						<li><a href="#">Уголок</a></li>
+						<li><a href="#">Швеллер</a></li>
+					</ul>
+				</div>
+				<div class="col-md-2 catbox li11">
+					<div class="itembox-zag">
+						<div class="imgbox">
+							<img src="img/cat20.png" alt="">
+						</div>
+						<a href="#">Клей плиточный</a>
+					</div>
+					<ul> 
+						<li><a href="#">Плиточный клей Юнис</a></li>
+						<li><a href="#">Плиточный клей Ceresit</a></li>
+						<li><a href="#">Плиточный клей Литокол</a></li>
+						<li><a href="#">Плиточный клей Кнауф</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
