b51ec66f06342d4394501470daefcd7250ce2c32
engine77 <zaur.ka.00@mail.ru>
2016-11-05 17:02:02 +0300

    5

---
+++ includes/test.php
@@ -0,0 +1,28 @@
+<br><br>
+<section class="df-bg">
+<div class="container">
+	<div class="df-menufixed">
+		<div class="df-logotip">
+			<div class="df-img-logo">
+				<img src="/img/dd-logo2.png" alt="">
+			</div>
+			<div class="dfimgtext">
+				<span>standart</span>
+				<span>resurs</span>
+			</div>
+			<div class="df-numbers">
+				<span>+7 (495)<em> 749 85 35 </em></span>
+				<span>+7 (919)<em> 997 90 20</em></span>			
+			</div>
+			<div class="df-button-buy">
+				<a href="#">Заказать звонок</a>
+			</div>
+			<div class="df-basket">
+				<span class="df-ellips">3</span>
+				<span class="df-pricebasket">5 450 <em>р.</em></span>
+			</div>
+		<div class="clear"></div>
+		</div>			
+	</div>
+</div>
+</section>
