7f51979d2884662457869a33d32c8412cbef5431
Залим Лампежев <sabolch@yandex.ru>
2016-11-08 17:14:37 +0300

    style6

--- cart.php
+++ cart.php
@@ -55,9 +55,11 @@
 			<div class="main-content-right">
 				<?php require_once('includes/cart-page.php'); ?>
 				<?php require_once('includes/newandpopular.php'); ?>
+
 			</div>
 		</div>
 	</section>
+	<?php require_once('includes/slider.php'); ?>
 	<?php require_once('includes/footer.php'); ?>
 	
 </body>
