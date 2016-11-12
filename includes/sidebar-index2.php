4332b1df1c430e363f4731dca9bd484e9167e06a
hatuhov <hatuhov@yandex.ru>
2016-11-12 10:59:58 +0300

    w

--- includes/sidebar-index2.php
+++ includes/sidebar-index2.php
@@ -3,7 +3,7 @@
 		<h3 class="db-h3 db-padd1">Подбор по параметрам</h3>
 		<li class="ah-menu-list_item">
 			<a href="#" class="ah-menu-list_item-link1 ah-font1">По цене:</a>
-			<ul class="ah-submenu-list ah-submenu-list1">
+			<ul style="display: block;" class="ah-submenu-list ah-submenu-list1">
 				<li class="db-input-li"><input type="text" id="minCost" value="0"/></li>
 				<li class="db-input-li1"><input type="text" id="maxCost" value="1000"/></li>
 				<div id="slider"></div>
